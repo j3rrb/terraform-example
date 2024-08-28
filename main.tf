d22ecf2c5a747c20be6ee18d5a8939987270966b
João Emanuel <55103007+j3rrb@users.noreply.github.com>
2024-08-27 22:42:03 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,10 +6,8 @@ resource "aws_instance" "example" {
   ami           = "ami-0e86e20dae9224db8"
   instance_type = "t2.micro"             
 
-  # Chave SSH para acessar a instância
   key_name = "teste-terraform"
 
-  # Tags para identificar a instância
   tags = {
     Name = "TesteTerraform"
   }
